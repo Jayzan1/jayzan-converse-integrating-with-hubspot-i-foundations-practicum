d931f01c13b6485d5448c4c98829030a3e322ae0
Jayzan1 <conversejayzan1@gmail.com>
2025-12-09 09:45:33 -0500

    Completed practicum: Added all routes, templates, and HubSpot API integration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,65 +8,61 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PORT = process.env.PORT || 3000;
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
+const CUSTOM_OBJECT_ID = process.env.HUBSPOT_CUSTOM_OBJECT_ID;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// Route 1: Homepage - list all Virtual Staff records
+app.get('/', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${process.env.HUBSPOT_CUSTOM_OBJECT_ID}?properties=name,role,department`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const resp = await axios.get(url, { headers });
+    const data = resp.data.results;
+    console.log(data); // <-- add this to see what properties are actually returned
+    res.render('homepage', { records: data });
+  } catch (err) {
+    console.error('Error fetching Virtual Staff:', err.response?.data || err.message);
+    res.send('Error fetching Virtual Staff records.');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// Route 2: Form to add new Virtual Staff
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Add New Virtual Staff' });
 });
-*/
 
+// Route 3: Handle form submission to create new record
+app.post('/update-cobj', async (req, res) => {
+  const { name, role, department } = req.body;
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_ID}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  const data = {
+    properties: {
+      name,
+      role,
+      department
+    }
+  };
+
+  try {
+    await axios.post(url, data, { headers });
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error creating Virtual Staff:', err.response?.data || err.message);
+    res.send('Error creating Virtual Staff record.');
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
